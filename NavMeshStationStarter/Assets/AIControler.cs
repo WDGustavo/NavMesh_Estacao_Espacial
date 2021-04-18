a306f0fbdf226846478ab47b125c4ac10ac3f36d
WDGustavo <54276455+WDGustavo@users.noreply.github.com>
2021-04-18 13:23:16 -0300

    NavMesh_RedAgent_Limit_Area

---
+++ NavMeshStationStarter/Assets/AIControler.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIControler : MonoBehaviour
+{
+    //pede para colcoar o Navmesh para pegar seu conteudo
+    public NavMeshAgent agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //pega o conteudo do navmeshagent do objeto
+        agent = this.GetComponent<NavMeshAgent>();
+    }
+ 
+    
+}
---
+++ NavMeshStationStarter/Assets/AgentManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentManager : MonoBehaviour
+{
+    //cria a lista
+    GameObject[] agents;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //acha o gameobject atraves da tag dele
+        agents = GameObject.FindGameObjectsWithTag("ai");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //faz a ação quando apertar o botão do mouse
+        if (Input.GetMouseButtonDown(0)) 
+        {
+            //cria o raycast
+            RaycastHit hit;
+
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+            {
+                //seta o destino do ogjeto quando clicar com o mouse
+                foreach (GameObject a in agents)
+                    a.GetComponent<AIControler>().agent.SetDestination(hit.point);
+            }
+        }
+
+    }
+}
